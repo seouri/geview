7a3cb26dd6c5f6e415f30ef20bc0a2182a1deb8f
Joon Lee <seouri@gmail.com>
2010-10-27 13:35:43 -0400

    show 1 graph per line

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
       data.push(value)
     end
     chr1_max = Histogram.where(:track_id => track).where(:level => level).where(:chromosome => "chr1").order("bin desc").limit(1)[0].bin
-    width = ((500 * bin_max).to_f / chr1_max).to_i
+    width = ((300 * bin_max).to_f / chr1_max).to_i
     Gchart.line(:title => chromosome, :size => "#{width}x80", :data => data, :format => 'image_tag').html_safe
   end
 end
