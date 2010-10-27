2c36be5285adb3675e1bb09d576ac6bfd38e6551
Joon Lee <seouri@gmail.com>
2010-10-27 13:39:13 -0400

    lower graph height

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,6 @@ module ApplicationHelper
     end
     chr1_max = Histogram.where(:track_id => track).where(:level => level).where(:chromosome => "chr1").order("bin desc").limit(1)[0].bin
     width = ((300 * bin_max).to_f / chr1_max).to_i
-    Gchart.line(:title => chromosome, :size => "#{width}x80", :data => data, :format => 'image_tag').html_safe
+    Gchart.line(:title => chromosome, :size => "#{width}x60", :data => data, :format => 'image_tag').html_safe
   end
 end
