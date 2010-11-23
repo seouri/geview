9d323d8f5c77bd798c6061bc9513d9b88800f997
Joon Lee <seouri@gmail.com>
2010-11-23 02:20:23 -0500

    link about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Geview::Application.routes.draw do
   resources :tracks
   match "/graph/:chromosome/:level(/:center)" => "page#graph", :as => :graph
+  match 'about' => "page#about"
   root :to => "page#home"
 end
